a55657d24f4be0d3c80cfe9b96b49ee5d559f0d4
John Dyer <lbrapid@gmail.com>
2013-12-16 11:33:45 -0500

    Specs for Jirafe::Client#send_cart

--- spec/lib/jirafe/client_spec.rb
+++ spec/lib/jirafe/client_spec.rb
@@ -5,17 +5,19 @@ describe Jirafe::Client do
     Jirafe::Client.new('123', '123')
   end
 
+  before(:each) do
+    @payload ={
+      'order' => Factories.order,
+      'original' => Factories.original,
+      'parameters' => Factories.parameters
+    }
+  end
 
   describe '#send_new_order' do
     before(:each) do
       Jirafe::CartBuilder.should_receive(:build_cart).and_call_original
       Jirafe::OrderBuilder.should_receive(:order_placed).at_least(:once).and_call_original
       Jirafe::OrderBuilder.should_receive(:order_accepted).and_call_original
-      @payload ={
-        'order' => Factories.order,
-        'original' => Factories.original,
-        'parameters' => Factories.parameters
-      }
     end
 
     context 'success' do
@@ -31,6 +33,29 @@ describe Jirafe::Client do
         @payload['order']['number'] = nil
         VCR.use_cassette('import_new_order_fail') do
           expect { subject.send_new_order(@payload) }.to raise_error(JirafeEndpointError)
+        end
+      end
+    end
+  end
+
+  describe '#send_cart' do
+    before(:each) do
+      Jirafe::CartBuilder.should_receive(:build_cart).and_call_original
+    end
+
+    context 'success' do
+      it 'sends the cart to jirafe' do
+        VCR.use_cassette('import_new_cart') do
+          subject.send_cart(@payload).should be_true
+        end
+      end
+    end
+
+    context 'failure' do
+      it 'raises JirafeEndpointError' do
+        @payload['order']['totals']['order'] = nil
+        VCR.use_cassette('import_new_cart_fail') do
+          expect { subject.send_cart(@payload) }.to raise_error(JirafeEndpointError)
         end
       end
     end
