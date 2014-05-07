2c1b7fdb2d387b666e96cac316c3717b76e7526a
Washington Luiz <huoxito@gmail.com>
2014-05-07 16:18:37 -0300

    Check before appending order totals info
    Some orders might not have any total at all besides email or user (o.O)

--- lib/jirafe_endpoint/cart_builder.rb
+++ lib/jirafe_endpoint/cart_builder.rb
@@ -2,21 +2,28 @@ module Jirafe
   class CartBuilder
     class << self
       def build_cart(payload)
-        {
+        cart = {
           'id' => payload['number'],
           'create_date' => payload['placed_on'],
           'change_date' => payload['updated_at'],
           'subtotal' => payload['item_total'].to_f,
-          'total' => payload['totals']['order'].to_f,
-          'total_tax' => payload['totals']['tax'].to_f,
-          'total_shipping' => payload['totals']['shipping'].to_f,
-          'total_payment_cost' => payload['totals']['payment'].to_f,
           'total_discounts' => 0,
           'currency' => payload['currency'],
           'items' => HashHelpers.items_hash(payload),
           'customer' => HashHelpers.cart_customer_hash(payload),
           'visit' => HashHelpers.visit_hash(payload)
         }
+
+        if payload['totals'].is_a? Hash
+          cart.merge!({
+            'total' => payload['totals']['order'].to_f,
+            'total_tax' => payload['totals']['tax'].to_f,
+            'total_shipping' => payload['totals']['shipping'].to_f,
+            'total_payment_cost' => payload['totals']['payment'].to_f,
+          })
+        end
+
+        cart
       end
     end
   end
