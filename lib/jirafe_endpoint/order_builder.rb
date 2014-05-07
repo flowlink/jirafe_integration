9217eada6e4d1a6bc773ebd3bb2da7314d982de1
Washington Luiz <huoxito@gmail.com>
2014-05-07 18:01:15 -0300

    Check all nested keys in OrderBuilder

--- lib/jirafe_endpoint/order_builder.rb
+++ lib/jirafe_endpoint/order_builder.rb
@@ -20,34 +20,51 @@ module Jirafe
       end
 
       def order_accepted(payload)
-        order_placed(payload).merge({
+        data = {
           'status' => 'accepted',
           'create_date' => payload['placed_on'],
           'change_date' => payload['updated_at'],
-          'subtotal' => payload['totals']['item'].to_f,
-          'total' => payload['totals']['order'].to_f,
-          'total_tax' => payload['totals']['tax'].to_f,
-          'total_shipping' => payload['totals']['shipping'].to_f,
-          'total_payment_cost' => payload['totals']['payment'].to_f,
           'total_discounts' => 0,
           'currency' => payload['currency'],
-          'delivery_address' => {
-            'postalcode' => payload['shipping_address']['zipcode'],
-            'address1' => payload['shipping_address']['address1'],
-            'country' => payload['shipping_address']['country'],
-            'state' => payload['shipping_address']['state'],
-            'city' => payload['shipping_address']['city']
-          },
+          'items' => HashHelpers.items_hash(payload),
+          'customer' => HashHelpers.order_customer_hash(payload)
+        }
+
+        if payload['totals'].is_a? Hash
+          data.merge!({
+            'subtotal' => payload['totals']['item'].to_f,
+            'total' => payload['totals']['order'].to_f,
+            'total_tax' => payload['totals']['tax'].to_f,
+            'total_shipping' => payload['totals']['shipping'].to_f,
+            'total_payment_cost' => payload['totals']['payment'].to_f,
+          })
+        end
+
+        if payload['shipping_address'].is_a? Hash
+          data.merge!({
+            'delivery_address' => {
+              'postalcode' => payload['shipping_address']['zipcode'],
+              'address1' => payload['shipping_address']['address1'],
+              'country' => payload['shipping_address']['country'],
+              'state' => payload['shipping_address']['state'],
+              'city' => payload['shipping_address']['city']
+            }
+          })
+        end
+
+        if payload['billing_address'].is_a? Hash
+          data.merge!({
           'payment_address' => {
             'postalcode' => payload['billing_address']['zipcode'],
             'address1' => payload['billing_address']['address1'],
             'country' => payload['billing_address']['country'],
             'state' => payload['billing_address']['state'],
             'city' => payload['billing_address']['city']
-          },
-          'items' => HashHelpers.items_hash(payload),
-          'customer' => HashHelpers.order_customer_hash(payload)
-        })
+            }
+          })
+        end
+
+        order_placed(payload).merge(data)
       end
     end
   end
